ca11e4e1caea6d7efe484bc81acf35d534f680fb
babyRess <139874554+babyRess@users.noreply.github.com>
2024-09-26 15:40:12 +0700

    update build config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
   output: 'export',
   distDir: 'build',
 };
